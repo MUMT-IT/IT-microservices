37dd5a761ab23e3dc835e45e6d7d3a6d4f8918d8
Likit Preeyanon <likit.pre@mahidol.edu>
2017-02-22 01:40:22 +0700

    added alembic migration scripts

---
+++ microservices/research/migrations/versions/3ee2b8f5ed09_updated_author_schema.py
@@ -0,0 +1,24 @@
+"""updated author schema
+
+Revision ID: 3ee2b8f5ed09
+Revises: 4627e8a56a23
+Create Date: 2017-02-22 00:01:23.985229
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3ee2b8f5ed09'
+down_revision = '4627e8a56a23'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ microservices/research/migrations/versions/4627e8a56a23_updated_scopus_affiliations_table.py
@@ -0,0 +1,24 @@
+"""updated scopus_affiliations table
+
+Revision ID: 4627e8a56a23
+Revises: 
+Create Date: 2017-02-21 21:49:46.439020
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4627e8a56a23'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
