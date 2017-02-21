631d452a193329b24c48e053dcad8b80ee61d97a
Likit Preeyanon <likit.pre@mahidol.edu>
2017-02-21 15:53:05 +0700

    added main.py and run.py

---
+++ microservices/research/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_restful import Api, Resource
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+import api.models
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+                'postgresql+psycopg2://likit@localhost/research_dev'
+
+    ma.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    #from api import employee_bp
+    #app.register_blueprint(employee_bp, url_prefix='/api')
+
+    return app
