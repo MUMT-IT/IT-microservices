6f2e3eeaf461071f3a962406595964a6dce5e42d
Likit Preeyanon <likit.pre@mahidol.edu>
2017-02-23 15:01:37 +0700

    added resource for affiliation list

--- microservices/employee/api/views.py
+++ microservices/employee/api/views.py
@@ -1,13 +1,15 @@
 from flask import jsonify
 from flask_restful import Api, Resource
 from . import employee_bp
-from models import Employee, EmployeeSchema
+from models import Employee, Affiliation, EmployeeSchema, AffiliationSchema
 from flask_restful.utils import cors
 
 api = Api(employee_bp)
 
 employee_schema = EmployeeSchema()
 employees_schema = EmployeeSchema(many=True)
+
+affils_schema = AffiliationSchema(many=True)
 
 class EmployeeResource(Resource):
     def get(self, id):
@@ -22,5 +24,14 @@ class EmployeeListResource(Resource):
         result = employees_schema.dump(empls).data
         return jsonify({'employees': result})
 
+
+class AffiliationListResource(Resource):
+    @cors.crossdomain(origin='*')
+    def get(self):
+        affils = Affiliation.query.all()
+        result = affils_schema.dump(affils).data
+        return jsonify({'affils': result})
+
+api.add_resource(AffiliationListResource, '/affils/')
 api.add_resource(EmployeeResource, '/employees/<int:id>')
 api.add_resource(EmployeeListResource, '/employees/')
