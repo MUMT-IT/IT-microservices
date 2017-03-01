d801c32ddf6f25aa13e7fe554c5aafae91cdac66
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-01 15:50:26 +0700

    deleted app

--- microservices/education/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from api import education_bp
-    app.register_blueprint(education_bp, url_prefix='/api')
-    return app
