6635e411a1963c065da5aa98ecf380758abf7560
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-01 23:09:09 +0700

    added get_credentials_from_file to get credentials directly from a file

--- microservices/education/api/drive.py
+++ microservices/education/api/drive.py
@@ -7,11 +7,16 @@ from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'api/client_secret.json'
 APPLICATION_NAME = 'MUMT-IT'
 
 def get_credentials():
+    try:
+        import argparse
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    except ImportError:
+        flags = None
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -19,6 +24,7 @@ def get_credentials():
     credential_path = os.path.join(credential_dir, 'drive-python-readonly.json')
     store = Storage(credential_path)
     credentials = store.get()
+
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
@@ -29,8 +35,19 @@ def get_credentials():
         print('Storing credentials to' + credential_path)
     return credentials
 
-def get_file_list(folder_id):
-    credentials = get_credentials()
+
+def get_credentials_from_file():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'drive-python-readonly.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    return credentials
+
+
+def get_file_list(folder_id, credentials):
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
     results = service.files().list(
@@ -39,7 +56,7 @@ def get_file_list(folder_id):
         fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
     if not items:
-        return {}
+        return []
     else:
         files = []
         for item in items:
@@ -48,5 +65,5 @@ def get_file_list(folder_id):
         return files
 
 if __name__=='__main__':
-    folder_id = '0BxLCeg0VgIlYcEhodkxpTEpwTVE'
-    get_file_list(folder_id)
+    credentials = get_credentials()
+    print(credentials.to_json())
