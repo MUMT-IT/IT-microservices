ab095b416d79753ec1481511dd5412ee05871035
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-01 16:10:33 +0700

    catching errors here break flask-script

--- microservices/education/api/drive.py
+++ microservices/education/api/drive.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+#from __future__ import print_function
 import httplib2
 import os
 
@@ -6,12 +6,6 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
