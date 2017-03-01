d6b3249726e2a24bec167f4d82dc62bab5c04b2d
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-01 23:10:25 +0700

    added secret key to the app

--- microservices/education/run.py
+++ microservices/education/run.py
@@ -1,8 +1,11 @@
+import os
 from flask_script import Manager
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    secret_key = os.getenv('FLASK_SECRET_KEY')
+    app.config['SECRET_KEY'] = secret_key
 
     from api import education_bp
     app.register_blueprint(education_bp, url_prefix='/api')
