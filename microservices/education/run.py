dd2495733759e0a455e98e9e875cf1c7c9a2ec7d
Likit Preeyanon <likit.pre@mahidol.edu>
2017-03-01 16:13:19 +0700

    merge main.py with run.py

--- microservices/education/run.py
+++ microservices/education/run.py
@@ -1,8 +1,17 @@
-from app import create_app
 from flask_script import Manager
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from api import education_bp
+    app.register_blueprint(education_bp, url_prefix='/api')
+
+    return app
 
 app = create_app()
 manager = Manager(app)
 
 if __name__ == '__main__':
     manager.run()
+    #app.run()
