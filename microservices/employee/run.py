85e6a43fe4eac29bde7815525f24cd40521d56c5
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-14 22:59:38 +0700

    added some variables to the shell context

--- microservices/employee/run.py
+++ microservices/employee/run.py
@@ -1,8 +1,16 @@
-from main import create_app
-from flask_script import Manager
+from main import create_app, mongo
+from flask_script import Manager, Shell
+from api.models import Employee, Department
 
 app = create_app()
 manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, mongo=mongo, Employee=Employee, Department=Department)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
 
 if __name__ == '__main__':
     manager.run()
