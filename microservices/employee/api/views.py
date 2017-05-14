84ac9fabcc2fa7c49f3c5e59e4ea0ea59dc7a588
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-14 15:35:47 +0700

    added employee list resource and profile image resource

--- microservices/employee/api/views.py
+++ microservices/employee/api/views.py
@@ -1,3 +1,5 @@
+import json
+from flask import request, make_response
 from flask_restful import Api, Resource
 from flask_restful.utils import cors
 from models import Department, Employee
@@ -20,5 +22,34 @@ class AffiliationResource(Resource):
         return data, 200, {'Content-type': 'application/json'}
 
 
+class EmployeeListResource(Resource):
+    @cors.crossdomain(origin='*')
+    def get(self):
+        dept_slug = request.args.get('department_slug')
+        print('dept slug is {}'.format(dept_slug))
+        dept = Department.objects(slug=dept_slug).first()
+        employees = Employee.objects(department=dept)
+        return employees.to_json(), 200, {'Content-type': 'application/json'}
+        '''
+        if dept_slug:
+            dept = Department.objects(slug=dept_slug).first()
+            employees = Employee.objects(department=dept)
+            return employees, 200, {'Content-type': 'application/json'}
+        else:
+            return jsonify(data={})
+        '''
+
+
+class ProfileImageResource(Resource):
+    @cors.crossdomain(origin='*')
+    def get(self, employee_id):
+        emp = Employee.objects(id=employee_id).first()
+        response = make_response(emp.photo.read())
+        response.mimetype = emp.photo.content_type
+        return response
+
+
 api.add_resource(AffiliationListResource, '/affiliations/')
 api.add_resource(AffiliationResource, '/affiliations/<ObjectId:object_id>')
+api.add_resource(EmployeeListResource, '/employees/')
+api.add_resource(ProfileImageResource, '/employees/image/<ObjectId:employee_id>')
