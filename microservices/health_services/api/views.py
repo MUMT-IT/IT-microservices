8088c036e636b2fd92c1761ad6cd60950cf7437b
Likit Preeyanon <likit.pre@mahidol.edu>
2017-05-17 10:16:17 +0700

    added new view for mobile health services' customer engagement

--- microservices/health_services/api/views.py
+++ microservices/health_services/api/views.py
@@ -2,6 +2,7 @@ from . import healthservice_blueprint as healthservice
 from sqlalchemy import create_engine, MetaData, Table, select, func
 from flask import jsonify
 from flask_cors import cross_origin
+from collections import defaultdict
 
 engine = create_engine('postgresql+psycopg2://likit@localhost/healthdw_dev',
                         convert_unicode=True)
@@ -11,6 +12,7 @@ con = engine.connect()
 
 facts = Table('facts', metadata, autoload=True)
 dates = Table('dates', metadata, autoload=True)
+companies = Table('companies', metadata, autoload=True)
 
 
 @healthservice.route('/customers/count/')
@@ -24,3 +26,26 @@ def get_annual_customers():
         rp = con.execute(s)
         data.append(dict(year=year, count=rp.scalar()))
     return jsonify({'data': data})
+
+
+@healthservice.route('/customers/companies/engagement/')
+@cross_origin(origin='*')
+def get_companies_engagement_rate():
+    data = []
+    total_counts = defaultdict(int)
+    for year in range(2008, 2018):
+        counts = []
+        s = select([companies.c.name.distinct()])
+        s = s.select_from(facts.join(dates).join(companies))
+        s = s.where(facts.c.service_date_id==dates.c.date_id)
+        s = s.where(facts.c.company_id==companies.c.company_id)
+        s = s.where(dates.c.gregorian_year==year)
+        rp = con.execute(s)
+        for c in rp:
+            total_counts[c[companies.c.name]] += 1
+            counts.append({
+                'company': c[companies.c.name],
+                'count': total_counts[c[companies.c.name]]
+            })
+        data.append(dict(year=year, value=counts))
+    return jsonify(data=data)
